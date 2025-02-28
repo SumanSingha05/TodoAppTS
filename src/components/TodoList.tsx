b0de892a448eabffacbffce3832583dab3afc239
SumanSingha05 <sumansingha8654@gmail.com>
2025-02-28 10:35:13 +0530

    todo delete button added

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,8 +1,26 @@
+import { Delete } from "lucide-react";
+import { Button } from "./ui/button";
+import { Card, CardHeader, CardTitle } from "./ui/card";
 
+type TodoListProps =  {
+  items : {id : string, text : string}[];
+}
 
-const TodoList = () => {
+const TodoList : React.FC<TodoListProps> = (props) => {
+
   return (
-    <div className="my-5">TodoList</div>
+    <div className="my-5">
+      {
+        props.items.map((todo) =>  (
+          <Card key={todo.id}>
+            <CardHeader className="flex flex-row items-center justify-between">
+              <CardTitle>{todo.text}</CardTitle>
+              <Button variant='destructive' size={'icon'}><Delete/></Button>
+            </CardHeader>
+          </Card>
+        ))
+      }
+    </div>
   )
 }
 
