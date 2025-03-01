0737364d8948afc9c396655a3a415f182c5750bb
SumanSingha05 <sumansingha8654@gmail.com>
2025-03-01 12:56:56 +0530

    server making done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,18 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import bodyParser from "body-parser";
 import todoRoutes from "./routes/todo";
 
 const app = express();
 const PORT = 8080;
+
+app.use(bodyParser. json());
+app.use("/todos", todoRoutes);
+
+//middleware to handle any other request
+app.use((err: Error, req: Request, res: Response, next : NextFunction) => {
+    res.status(500).json({message:err.message});
+
+})
 
 app.listen(PORT, () => {
     console.log(`Server is listen at port ${PORT}`);
--- backend/src/routes/todo.ts
+++ backend/src/routes/todo.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import { createTodo } from "../controllers/todo";
 
 const router = express.Router();
 
-router.route("/").post() //create todo
+router.route("/").post(createTodo) //create todo
 router.route("/").get() // get todo
 router.route("/:todoId").patch() // update todo
 router.route("/:todoId").delete() // get todo
